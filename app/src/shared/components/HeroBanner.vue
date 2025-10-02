3f8b5f9f00427947d20842b980f3c68bf36d2026
smartdevau@outlook.com <franco.diazlicham@hotmail.com>
2025-10-02 21:56:50 +1000

    Added more mappers and header component

---
+++ app/src/shared/components/HeroBanner.vue
@@ -0,0 +1,14 @@
+<template>
+    <section id="hero" class="w-100 my-2 rounded-3 shadow bg-primary-subtle">
+        <div class="p-5 text-center">
+            <h1 class="mb-1">{{ title }}</h1>
+            <p class="lead mb-0">{{ subtitle }}</p>
+        </div>
+    </section>
+</template>
+<script setup lang="ts">
+defineProps({
+    title: { type: String, required: true },
+    subtitle: { type: String, required: true },
+});
+</script>
